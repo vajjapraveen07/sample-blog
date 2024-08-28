130f049668c57569c27cbcc988739b9350fbf1ab
vajjapraveen07 <praveenvajja97@gmail.com>
2024-08-28 19:22:55 +0530

    this is my task 2

---
+++ README.md
@@ -0,0 +1,2 @@
+This is my blog 
+I created This blog 
---
+++ admin/profile.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include("../connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div style="text-align:center; padding:15%;">
+      <p  style="font-size:50px; font-weight:bold;">
+       Hello  <?php 
+       if(isset($_SESSION['email'])){
+        $email=$_SESSION['email'];
+        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
+        while($row=mysqli_fetch_array($query)){
+            echo $row['firstName'].' '.$row['lastName'];
+        }
+       }
+       ?>
+       :)<br />
+       <h2>Welcome to Dashboard</h2><br />
+       <a class="btn btn-primary" href="index.php">Click Here</a>
+      </p>
+      
+    </div>
+</body>
+</html>
